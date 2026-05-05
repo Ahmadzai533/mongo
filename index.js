b839848d5bddc409b232ede2dea1acecc2658b05
Roohullah Ahmadzai <roohullahpoohan@gmail.com>
2026-05-05 22:59:30 +0430

    create a new user to mongo

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-const mongoose = require('mongoose');
-main().then(()=>{console.log("connected seccessfully")})
-.catch(err => console.log(err));
+const mongoose = require("mongoose");
+main()
+  .then(() => {
+    console.log("connected seccessfully");
+  })
+  .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+}
 
-  }
+const newSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  city: String,
+});
 
-  const newSchema = new mongoose.Schema({
-    name:String,
-    age:Number,
-    city:String
+const User = mongoose.model("User", newSchema);
 
-  })
-
-const User = mongoose.model('User',newSchema);
-
-const Employee = mongoose.model("Employee",newSchema);
+const user1 = new User({
+  name: "Sultan",
+  age: 23,
+  city: " Nangrahar",
+});
+user1.save().then((res)=>{console.log(res)}).catch((err)=>{console.log(err)});
