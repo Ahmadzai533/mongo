1a74fbe5890eedaaeb1de156b26f848d249bccb6
Roohullah Ahmadzai <roohullahpoohan@gmail.com>
2026-05-05 22:25:11 +0430

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+main().then(()=>{console.log("connected seccessfully")})
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+  }
+
+  const newSchema = new mongoose.Schema({
+    name:String,
+    age:Number,
+    city:String
+  })
